639dadd8a29b4c02319cc303e7c1e244a1924b5e
MM Yap <mryap2@up.edu.ph>
2026-05-18 21:28:22 +0800

    [GREEN] Implement task creation route

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,5 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
+
+const tasks = require("../data/tasks");
+const validateTask = require("../utils/taskValidation");
+
+router.post("/", (req, res) => {
+  const { title } = req.body;
+
+  const task = { title };
+
+  if (!validateTask(task)) {
+    return res.status(400).json({
+      error: "Invalid task title",
+    });
+  }
+
+  tasks.push(task);
+
+  return res.status(201).json(task);
+});
 
 module.exports = router;
